05fa1ed39dee9b86f5bc7b079c570468b713ad38
Josh Pollock <josh@calderawp.com>
2018-10-31 16:36:48 -0400

    404 if no post found

--- src/index.js
+++ src/index.js
@@ -6,14 +6,26 @@ app.get("/", (req, res) => {
   res.send(`Hi Roy!`);
 });
 
+const notFound = {
+	code: "rest_post_invalid_id",
+	message: "Invalid post ID.",
+	data: {
+		status: 404
+	}
+};
 const wpContentPath =  '/content/wp-json';
 app.get("/posts/:postName", (req, res) => {
 	const cwd = process.cwd();
   	const {postName} = req.params;
   	const path = cwd + wpContentPath + '/posts/' + postName + '.json';
   	const exists = fs.existsSync(path);
-  	const post = require(path);
-	res.json(post.data);
+	if (exists) {
+		const post = require(path);
+		res.json(post.data);
+	} else {
+		res.status( 404 );
+		res.json(notFound);
+	}
 });
 const server = app.listen(process.env.PORT, () => {
   console.log("Started at http://localhost:%d\n", server.address().port);
