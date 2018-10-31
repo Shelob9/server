cce5131677ef0ded064b77e7caa1b97329e5ef11
Josh Pollock <josh@calderawp.com>
2018-10-31 16:34:46 -0400

    serve stored json if it exists.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require( 'express' );
-
 const app = express();
 
+const fs = require('fs');
 app.get("/", (req, res) => {
   res.send(`Hi Roy!`);
 });
 
+const wpContentPath =  '/content/wp-json';
+app.get("/posts/:postName", (req, res) => {
+	const cwd = process.cwd();
+  	const {postName} = req.params;
+  	const path = cwd + wpContentPath + '/posts/' + postName + '.json';
+  	const exists = fs.existsSync(path);
+  	const post = require(path);
+	res.json(post.data);
+});
 const server = app.listen(process.env.PORT, () => {
   console.log("Started at http://localhost:%d\n", server.address().port);
 });
